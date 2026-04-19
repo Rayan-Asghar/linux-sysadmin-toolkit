cfec3d59258fda5951feebb81a3a551e9a330422
Rayan-Asghar <rayanasghar.dev@gmail.com>
2026-04-19 20:17:04 +0500

    chore:Completed the disk monitor script

---
+++ scripts/disk-monitor.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#======================================
+#Script Name: disk-monitor.sh
+#Description: Monitors disk usage on all mounted partitions.
+#              Logs results with timestamps. Alerts to terminal
+#              when usage exceeds the defined threshold.
+#Author: Rayan Asghar
+#Date: April 19, 2026
+#Version: 0.0.1
+#Usage: ./disk-monitor.sh
+#======================================
+
+#Location of log file
+LOG_DIR="$HOME/logs"
+
+#List log file
+LOG_FILE="disk-monitor.log"
+
+#Full path to the log file
+LOG_PATH="$LOG_DIR/$LOG_FILE"
+
+#Threshold Value
+THRESHOLD=60
+
+
+#=========================================
+#Setup
+
+mkdir -p "$LOG_DIR"
+
+# Capture current timestamp for log entries
+DATE=$(date '+%Y-%m-%d %H:%M:%S' )
+
+#To Track If any partition breached the threshold
+ALERT_FLAG="/tmp/disk-alert-flag"
+rm -f "$ALERT_FLAG"
+
+#======================================================
+#Disk Check
+df -h | grep -vE '^Filesystem|tmpfs|udev' | while read -r LINE; do 
+    USAGE=$(echo "$LINE" | awk '{print $5}' |cut -d'%' -f1)
+    PARTITION=$(echo "$LINE" | awk '{print $6}')
+    if [ "$USAGE" -ge "$THRESHOLD" ]; then
+        echo "[$DATE] WARNING:$PARTITION is at ${USAGE}% - ABOVE THRESHOLD" >>"$LOG_PATH"
+        echo "WARNING:$PARTITION is at ${USAGE}% -ABOVE THRESHOLD"
+        touch "$ALERT_FLAG"
+    else
+        echo "[$DATE] OK: $PARTITION is at ${USAGE}%">> "$LOG_PATH"
+    fi
+
+done
+
+if [ -f "$ALERT_FLAG" ]; then
+    rm -f "$ALERT_FLAG"
+    exit 1
+fi
+
+exit 0
